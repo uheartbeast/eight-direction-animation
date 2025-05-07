724a6952ef60d628bcd18ed693c96cbb551d22d2
Heartbeast <heartbeast.studios@gmail.com>
2025-05-07 13:38:52 -0600

    3 - Animation Tree

--- player.gd
+++ player.gd
@@ -4,6 +4,11 @@ const SPEED = 90
 
 var input_vector: = Vector2.ZERO
 
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+func _ready() -> void:
+	animation_tree.set("parameters/TimeScale/scale", 0.1)
+
 func _physics_process(delta: float) -> void:
 	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
